097e614e2c6bf3ff4544b0fe007b0ec0c0f2aca7
sarah benzi <sarahlover822@gmail.com>
2023-03-15 19:45:06 -0300

    feita configuracao de salvamento de upload de arquivos

--- index.js
+++ index.js
@@ -24,3 +24,15 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}))
 app.use(cors());
 app.use("/assets",express.static(path.join(__dirname,"public/assets")))
+
+
+/* FILE STORAGE */
+
+const storage = multer.diskStorage({
+    destination:function(req,file,cb){
+        cb(null,"public/assets")
+    },
+    filename:function(req,file,cd){
+        cb(null,file.originalname)
+    }
+})
